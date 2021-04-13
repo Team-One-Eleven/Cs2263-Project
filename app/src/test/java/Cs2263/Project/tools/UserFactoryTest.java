3971a30a45159702f8a636479496f81a571f181c
BraydonS <braydonspaulding1@gmail.com>
2021-04-13 15:09:10 -0600

    please work

---
+++ app/src/test/java/Cs2263/Project/tools/UserFactoryTest.java
@@ -0,0 +1,48 @@
+/**
+ * User Factory class
+ *
+ * Produces a new userInfo, called from orchestrator.registerUser()
+ *
+ * Produces a new User using userInfo.
+ *
+ *
+ * @author  Traae
+ * @version 1.0
+ * @since 3/31/2021
+ */
+
+package Cs2263.Project.tools;
+
+import Cs2263.Project.ConfigurationTest;
+import Cs2263.Project.OrchestratorTest;
+import Cs2263.Project.listable.UserCredentials;
+
+
+public class UserFactoryTest {
+//
+//
+//    private OrchestratorTest orchestrator;
+//
+//    // Constructor
+//    public UserFactoryTest(OrchestratorTest o){
+//        this.orchestrator = o;
+//    }
+//
+//    // Methods
+//    // Makes
+//    public User makeUser(){
+//        User u = new User();
+//        return new User();
+//    }
+//    public UserCredentials makeUserInfo(String email, String password, double userId) {
+//        UserCredentials newInfo = new UserCredentials();
+//        newInfo.setUserEmail(email);
+//        newInfo.setUserPassword(password);
+//        newInfo.setUserId(userId);
+//        newInfo.setUserFile(ConfigurationTest.USER_DATA_BASE + userId + ConfigurationTest.USER_DATA_FILE_SUFFIX);
+//        return newInfo;
+//    }
+//
+//
+
+}
