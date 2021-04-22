d1b64c24d847963bad4400b81edc13ebd60a4afe
Traae <bloxtraa@isu.edu>
2021-04-22 12:24:46 -0600

    git conflicts

--- app/src/main/java/Cs2263/UI/UIView.java
+++ app/src/main/java/Cs2263/UI/UIView.java
@@ -65,7 +65,7 @@ public class UIView extends Application {
         primaryStage.setTitle("TODO");
 
         //Start at login scene and show
-        primaryStage.setScene(registerScene);
+        primaryStage.setScene(loginScene);
         primaryStage.show();
     }
 
