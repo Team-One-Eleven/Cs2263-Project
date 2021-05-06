33b7059108ce57916c8ebcde9e727cfa4979260d
Traae <bloxtraa@isu.edu>
2021-05-06 12:30:43 -0600

    Quick save

---
+++ app/src/main/java/Cs2263/Project/UseableDate.java
@@ -0,0 +1,24 @@
+//package Cs2263.Project;
+//
+//import java.io.Serializable;
+//import java.time.LocalDate;
+//import java.time.Month;
+//
+//public class UseableDate implements Serializable {
+//    private int dayOfMonth;
+//    private Month month;
+//    private int year;
+//
+//
+//    public UseableDate(){
+//        LocalDate ld = LocalDate.now();
+//        this.dayOfMonth = ld.getDayOfMonth();
+//        this.month = ld .getMonth();
+//        this.year = ld.getYear();
+//    }
+//
+//    public boolean isAfter(UseableDate toCompare){
+//
+//    }
+//
+//}
